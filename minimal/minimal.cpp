e656a676c0db33dc109d2de1265806befa1a3d56
David Haworth <dh@thelancashireman.org>
2020-12-11 21:20:44 +0100

    Fix GPL notice

--- minimal/minimal.cpp
+++ minimal/minimal.cpp
@@ -2,20 +2,20 @@
  *
  * (c) 2020 David Haworth
  *
- * modular is free software: you can redistribute it and/or modify
+ * minimal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * modular is distributed in the hope that it will be useful,
+ * minimal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with modular.  If not, see <http://www.gnu.org/licenses/>.
+ * along with minimal.  If not, see <http://www.gnu.org/licenses/>.
  *
- * modular is an Arduino sketch, written for an Arduino Nano
+ * minimal is an Arduino sketch, written for an Arduino Nano
 */
 #include <Arduino.h>
 
